c5f07c9d0abd091e2c5c5f0fa72d0d4548be5bbc
Diego Aldao <diego_ind@live.com.ar>
2022-08-02 16:56:01 -0300

    agregada la variable de enviroment y moificada la ruta

--- src/app/servicios/usuario.service.ts
+++ src/app/servicios/usuario.service.ts
@@ -1,17 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Usuario } from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
-  URL = "http://localhost:8080/usuarios/";
+  private urlApiLocal= environment.URLapi;
 
   constructor(private http: HttpClient) { }
 
   public getUsuario(): Observable<Usuario>{
-    return this.http.get<Usuario>(this.URL +"id/4")
+    return this.http.get<Usuario>(`${this.urlApiLocal}/usuario/id/3`)
   }
 }
