703404945cf8e697ff18eb3b896064802432c992
Diego Aldao <diego_ind@live.com.ar>
2022-08-02 16:25:28 -0300

    cambiada la exportacion de clase a interface y agregados los datos faltantes

--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -1,12 +1,9 @@
-export class Usuario{
-    id?: number;
+export interface Usuario{
+    id: number;
     nombre: String;
     apellido: String;
     img: String;
-
-    constructor(nombre: String, apellido: String, img: String){
-        this.nombre= nombre;
-        this.apellido = apellido;
-        this.img = img;
-    }
+    titulo: String;
+    descripcion: String;
+    imgPortada: String
 }
