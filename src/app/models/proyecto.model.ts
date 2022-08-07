0974e2ca5f0f7fa16f8334c310274f208a05f3d7
Diego Aldao <diego_ind@live.com.ar>
2022-08-07 16:46:47 -0300

    añadido el modelo de proyecto

---
+++ src/app/models/proyecto.model.ts
@@ -0,0 +1,12 @@
+export interface Proyecto{
+    idPro: number;    
+    tituloPro: String;    
+    fechaPro: number;    
+    descripcionPro: String;    
+    imgPro: String;    
+    linkCode: String;    
+    linkSitio: String;    
+    mainTecno: String;    
+    subTecnoA: String;    
+    subTecnoB: String;
+}
