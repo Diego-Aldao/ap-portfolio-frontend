224bbe18f0c75081684f76a3d7d5128a19cc3d00
Diego Aldao <diego_ind@live.com.ar>
2022-08-07 15:09:19 -0300

    añadido el modelo de educacion

---
+++ src/app/models/educacion.model.ts
@@ -0,0 +1,10 @@
+export interface Educacion{
+    idEdu: number;
+    institucionEdu: String;
+    tituloEdu: String;
+    fechaIniEdu: number;
+    fechaFinEdu: number;
+    descripcionEdu: String;
+    linkEdu: String;
+    imgEdu: String;
+}
