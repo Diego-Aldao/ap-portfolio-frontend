4d06d122827d3ef75bc8fa4bb19957dfb6ba1df5
Diego Aldao <diego_ind@live.com.ar>
2022-08-07 15:56:12 -0300

    añadido el metodo getEducaciones

--- src/app/componentes/habilidades/habilidades.component.ts
+++ src/app/componentes/habilidades/habilidades.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Habilidad } from 'src/app/models/habilidad.model';
+import { HabilidadService } from 'src/app/servicios/habilidad.service';
 
 @Component({
   selector: 'app-habilidades',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HabilidadesComponent implements OnInit {
 
-  constructor() { }
+  public habilidades:Habilidad[]=[];
+
+  constructor(private habilidadService:HabilidadService) { }
 
   ngOnInit(): void {
+    this.getEducaciones();
+  }
+
+  public getEducaciones():void{
+    this.habilidadService.getHabilidad().subscribe(
+      data =>{
+        this.habilidades = data;
+      }
+    )
   }
 
 }
