19cbeb713bd71224250e190042c0d41359888d87
Diego Aldao <diego_ind@live.com.ar>
2022-08-17 17:52:50 -0300

    creado el servicio de autenticacion

---
+++ src/app/servicios/autenticacion.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacionService {
+  url= environment.URLapi;
+  currentUserSubject: BehaviorSubject<any>;
+  constructor(private http:HttpClient) { 
+    this.currentUserSubject = new BehaviorSubject<any>(JSON.parse(sessionStorage.getItem("currentUser") || "{}"))
+  }
+
+  iniciarSesion(credenciales:any):Observable<any>{
+    return this.http.post(this.url + "/login", credenciales).pipe(map(data=>{
+      sessionStorage.setItem("currentUser", JSON.stringify(data));
+      return data
+    }))
+  }
+
+  get UsuarioAutenticado()
+  {
+    return this.currentUserSubject.value;
+  }
+}
