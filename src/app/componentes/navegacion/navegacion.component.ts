9b334092731d269405eee7156505be1fce5ac40f
Diego Aldao <diego_ind@live.com.ar>
2022-08-17 19:08:49 -0300

    añadido el servicio de autenticacion para saber si el usuario esta logueado y añadidas las funciones de login y logout

--- src/app/componentes/navegacion/navegacion.component.ts
+++ src/app/componentes/navegacion/navegacion.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
 
 @Component({
   selector: 'app-navegacion',
   templateUrl: './navegacion.component.html',
   styleUrls: ['./navegacion.component.css']
 })
-export class NavegacionComponent implements OnInit {
-
-  constructor(private router:Router) { }
-
+export class NavegacionComponent implements OnInit {  
+  
+  constructor(private ruta:Router, public autenticacionService:AutenticacionService) { 
+  }
+  
   ngOnInit(): void {
   }
   
-  login(){
-    this.router.navigate(["/login"])
+  isLogged = this.autenticacionService.UsuarioLogueado;
+  
+  login(): void{
+    this.ruta.navigate(["/login"])
+  }
+
+  logout(): void{
+    window.location.reload();
+    sessionStorage.clear();
+    this.isLogged = false;
   }
 
 }
