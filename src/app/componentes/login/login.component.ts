ff9ec6f4fd2ab4a9c49cedc4057a742f7acc297f
Diego Aldao <diego_ind@live.com.ar>
2022-08-17 17:56:40 -0300

    añadidas las validaciones del form y la funcion a ejecutar en el Submit

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  form:FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder, private autenticacionService:AutenticacionService, private ruta:Router) { 
+    this.form=this.formBuilder.group(
+      {
+        email:["",[Validators.required, Validators.email]],
+        password:["",[Validators.required, Validators.minLength(5)]],
+      }
+    )
+  }
 
   ngOnInit(): void {
   }
 
+  get Email()
+  {
+    return this.form.get("email");
+  }
+  
+  get Password()
+  {
+    return this.form.get("password");
+  }
+
+  onEnviar(event:Event){
+    event.preventDefault;
+    this.autenticacionService.iniciarSesion(this.form.value).subscribe(data=>{
+      console.log("DATA: " + JSON.stringify(data));
+      this.ruta.navigate([""])
+    })
+  }
+ 
 }
